533b41421e828199fbefc21bfc92cc20f9b299ce
Wehbiels <Lara.Wehbie@gmail.com>
2018-05-30 15:03:46 -0400

    add

--- server.js
+++ server.js
@@ -7,43 +7,42 @@ const path = require('path');
 var axios = require("axios");
 
 
-// require models
+
 var db = require("./models");
 
 var PORT = 3000;
 
-// initialize express
+
 var app = express();
 
-// middleware
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-// routes
 
-const scrape = require("./routes/scrap.js");
-const handlebars = require("./routes/handlebars.js")
+
+var scrape = require("./routes/scrap.js");
+var handlebars = require("./routes/handlebars.js")
 
 app.use("/", scrape);
 app.use("/", handlebars);
 
-// express handlebars
+
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "handlebars");
 
-// static directory
+
 app.use(express.static("public"));
 
 
-// connect to mongo database
+
 mongoose.connect("mongodb://localhost/scraper");
 
 
 
-// start server
 app.listen(PORT, function () {
 	console.log("App running on port " + PORT + "!");
 });
